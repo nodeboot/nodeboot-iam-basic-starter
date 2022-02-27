f712ea6ea2a9075b5c8e23894e02c97153dc282f
jrichardsz <jrichardsz.java@gmail.com>
2022-02-26 20:04:00 -0500

    succes database init test

--- src/main/services/data/DatabaseHelperDataService.js
+++ src/main/services/data/DatabaseHelperDataService.js
@@ -1,25 +1,46 @@
 @Service
 function DatabaseHelperDataService() {
 
-  @Autowire(name = "dbSession")
-  this.dbSession;
-
-  this.init = () => {
+  this.init = (dbSession) => {
     return new Promise(async (resolve, reject) => {
-      try {
+      if(typeof dbSession === 'undefined'){
+        reject(new Error("is not possible stabilish a database connection"))
+        return;//TODO: why return is required to end this reject execution
+      }
 
-        var params = {
-          resource: resource,
-          permission: permission,
-          role: role
-        };
-
-        var countPermissions = await this.dbSession
-          .raw(this.hasPermissionSqlString, params);
-        resolve(countPermissions[0][0]);
-      } catch (err) {
-        console.log(err);
-        reject("Failed to find next image");
+      var existsSubjectTable = await dbSession.schema.hasTable('iam_subject');
+      var existsIamPermissionTable = await dbSession.schema.hasTable('iam_permission');
+      if (existsSubjectTable === true && existsIamPermissionTable === true) {
+        console.log("iam tables already exist");
+        resolve(true);
+      }else if (existsSubjectTable === false && existsIamPermissionTable === true) {
+        console.log("iam tables are imcomplete. iam_subject table don't exist");
+        resolve(false);
+      }else if (existsIamPermissionTable === false && existsSubjectTable === true) {
+        console.log("iam tables are imcomplete. iam_permission table don't exist");
+        resolve(false);
+      }else {
+        console.log("iam tables don't exist. Creating...");
+        dbSession.createTable('iam_subject', async (table)=>{
+          table.increments('id').primary();
+          table.string('identifier', 25).unique().notNullable();
+          table.string('secret', 255).notNullable();
+          table.string('role', 50).notNullable();
+        }).then(async function(){
+          dbSession.createTable('iam_permission', async (table)=>{
+            table.increments('id').primary();
+            table.string('role', 25).notNullable();
+            table.string('resource', 50).notNullable();
+            table.string('permission', 150).notNullable();
+          }).then(async function(){
+            var iamPermissionTable = await dbSession.schema.hasTable('iam_permission');
+            var userTable = await dbSession.schema.hasTable('iam_subject');
+            if(iamPermissionTable === false || userTable === false){
+              reject(new Error("tables did not exist and and could not be created"));
+            }
+            resolve(true)
+          })
+        });
       }
     });
   }
---
+++ src/test/services/data/DatabaseHelperDataService.test.js
@@ -0,0 +1,187 @@
+const NodeInternalModulesHook = require('meta-js').NodeInternalModulesHook;
+NodeInternalModulesHook._compile();
+
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert;
+
+const DatabaseHelperDataService = require("../../../../src/main/services/data/DatabaseHelperDataService.js");
+
+describe('DatabaseHelperDataService: init', function() {
+  it('should throw an  error if dabatase sesion is null or undefined', async function() {
+    var databaseHelperDataService = new DatabaseHelperDataService();
+    var ex;
+    try {
+      await databaseHelperDataService.init();
+    } catch (e) {
+      ex = e;
+    }
+    assert(ex, "The error should have been thrown");
+  });
+
+  it('should return true if tables already exist', async function() {
+
+    var schema = function(){
+      return new function(){
+        this.hasTable = function(){
+          return new Promise((resolve, reject) => {
+            resolve(true)
+          })
+        }
+      }
+    }
+
+    var dbSession = function(){
+      this.schema = new schema();
+    }
+
+    var databaseHelperDataService = new DatabaseHelperDataService();
+    var result = await databaseHelperDataService.init(new dbSession());
+    expect(result).to.equal(true);
+  });
+
+  it('should return false if iam_permission dont exist', async function() {
+
+    var schema = function(){
+      return new function(){
+        this.hasTable = function(tableName){
+          return new Promise((resolve, reject) => {
+            resolve(tableName=="iam_subject")
+          })
+        }
+      }
+    }
+
+    var dbSession = function(){
+      this.schema = new schema();
+    }
+
+    var databaseHelperDataService = new DatabaseHelperDataService();
+    var result = await databaseHelperDataService.init(new dbSession());
+    expect(result).to.equal(false);
+  });
+
+  it('should return false if iam_subject dont exist', async function() {
+
+    var schema = function(){
+      return new function(){
+        this.hasTable = function(tableName){
+          return new Promise((resolve, reject) => {
+            resolve(tableName=="iam_permission")
+          })
+        }
+      }
+    }
+
+    var dbSession = function(){
+      this.schema = new schema();
+    }
+
+    var databaseHelperDataService = new DatabaseHelperDataService();
+    var result = await databaseHelperDataService.init(new dbSession());
+    expect(result).to.equal(false);
+  });
+
+  it('should throw an error if tables dont exist but they were not created', async function() {
+
+    function table(){
+    }
+
+    function increments(){
+      return new function(){
+          this.primary = function(){}
+      };
+    }
+
+    function string(){
+      return new function(){
+          this.unique = function(){
+            return new function(){
+              this.notNullable = function(){}
+            }
+          }
+          this.notNullable = function(){}
+      };
+    }
+
+    table.increments = increments;
+    table.string = string;
+
+    var schema = function(){
+      this.hasTable = function(tableName){
+        return new Promise((resolve, reject) => {
+          resolve(false)
+        })
+      }
+    }
+
+    var dbSession = function(){
+      this.schema = new schema();
+      this.createTable = async function(tableName, callback){
+        callback(table)
+      }
+    }
+
+    var databaseHelperDataService = new DatabaseHelperDataService();
+
+    try {
+      await databaseHelperDataService.init(new dbSession());
+    } catch (e) {
+      ex = e;
+    }
+    assert(ex, "The error should have been thrown");
+  });
+
+  it('should return true if tables dont exist and they were created', async function() {
+
+    function table(){
+    }
+
+    function increments(){
+      return new function(){
+          this.primary = function(){}
+      };
+    }
+
+    function string(){
+      return new function(){
+          this.unique = function(){
+            return new function(){
+              this.notNullable = function(){}
+            }
+          }
+          this.notNullable = function(){}
+      };
+    }
+
+    table.increments = increments;
+    table.string = string;
+
+    var hasTableCallsCount=0;
+
+    var schema = function(){
+      this.hasTable = function(tableName){
+        return new Promise((resolve, reject) => {
+          if(hasTableCallsCount<2){
+            resolve(false)
+          }else{
+            resolve(true)
+          }
+          hasTableCallsCount++;
+        })
+      }
+    }
+
+    var dbSession = function(){
+      this.schema = new schema();
+      this.createTable = async function(tableName, callback){
+        callback(table)
+      }
+    }
+
+    var databaseHelperDataService = new DatabaseHelperDataService();
+     var result = await databaseHelperDataService.init(new dbSession());
+     expect(result).to.equal(true);
+  });
+
+});
